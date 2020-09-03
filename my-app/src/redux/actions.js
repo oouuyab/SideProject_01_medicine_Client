2c6027267d1a4142f445adb9f1f92f56c2cf09b6
oouuyab <01073785645@naver.com>
2020-09-04 00:56:52 +0900

    :construction: 검색 기능 작성 중
    redux 추가 및 search 기능 작성 중

---
+++ my-app/src/redux/actions.js
@@ -0,0 +1,6 @@
+// import SEARCH_KEYWORD from './actionType';
+
+// export const searchKeyword = (keyword) => ({
+//   type: SEARCH_KEYWORD,
+//   keyword
+// });
---
+++ my-app/src/redux/reducers/medicines.js
@@ -0,0 +1,17 @@
+const medicinesInitialState = {
+  title: 'My MedicinesList',
+  medicines: [],
+  keyword: ''
+};
+
+export const medicineReducer = (state = medicinesInitialState, action) => {
+  // switch (action.type) {
+  //   case SEARCH_KEYWORD:
+  //     return {
+  //       ...state,
+  //       keyword: action.keyword
+  //     };
+  //   default:
+  return state;
+  // }
+};
