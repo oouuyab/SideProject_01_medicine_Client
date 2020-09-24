88e69836b6d9292239aed9984006c8bc51821475
oouuyab <01073785645@naver.com>
2020-09-24 23:57:40 +0900

    :construction: resultList 작성 중
    resultList 작성 중

--- my-app/src/components/resultList.js
+++ my-app/src/components/resultList.js
@@ -4,20 +4,32 @@ const ResultList = (props) => {
   try {
     const { currentList, getContraindicate } = props;
 
-    getContraindicate(currentList);
+    let list = [];
 
-    // function makeList(item) {
-    //   return (
-    //     <div key={item[0] + item[1]}>
-    //       <div key={item[0]}>{item[0]}</div>
-    //       <div key={item[1]}>{item[1]}</div>
-    //     </div>
-    //   );
-    // }
-    // return currentList.map((item) => {
-    // return makeList();
-    // });
-    return <></>;
+    for (let i = 0; i < currentList.length - 1; i++) {
+      for (let j = i + 1; j < currentList.length; j++) {
+        list.push([currentList[i], currentList[j]]);
+      }
+    }
+    let resultList = list.map((items) => {
+      let result = getContraindicate(items);
+      return (
+        <div>
+          <div>제조사</div>
+          <div>{result.ENTP_NAME}</div>
+          <div>품목 명</div>
+          <div>{result.ITEM_NAME}</div>
+          <div>제조사</div>
+          <div>{result.MIXTURE_ENTP_NAME}</div>
+          <div>품목 명</div>
+          <div>{result.MIXTURE_ITEM_NAME}</div>
+          <div>금기내용</div>
+          <div>{result.PROHBT_CONTENT}</div>
+        </div>
+      );
+    });
+
+    return <>{resultList}</>;
   } catch (err) {
     console.log('CurrentList err');
     console.log(err);
